b85f703fb7488a7c2b9aaa28def380fbb4cea852
CodeExplorer4267 <rupambhadra478@gmail.com>
2025-08-12 23:42:57 +0530

    commit5

---
+++ ControlFlow/script.js
@@ -0,0 +1,28 @@
+// function getGrade(score){
+//     if(score >= 90 && score<=100){
+//          return "A+";
+//     }
+//     else if(score >= 80 && score<=89){
+//         return "A";
+//     }
+//     else if(score >= 70 && score<=79){
+//         return "B";
+//     }
+//     else if(score >= 60 && score<=69){
+//         return "C";
+//     }
+//     else if(score >= 33 && score<=59){
+//         return "D";
+//     }
+//     else if(score >= 0 && score <= 32){
+//         return "Fail";
+//     }
+//     else{
+//         return "Invalid Input"
+//     }
+// }
+// console.log(getGrade(74))
+
+//early return pattern is if the if block have 1 line we can directly write in one line
+// let a=81;
+// if(a==81) console.log("jdfs")
---
+++ Loops/script.js
@@ -0,0 +1,3 @@
+// if we know from where we should start and to where we should go -> for loop
+//if we know from where we should start and when we should stop -> while
+ 
