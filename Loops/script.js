bc1ea31cb921c72bdfeba7c44117474dd928555f
CodeExplorer4267 <rupambhadra478@gmail.com>
2025-08-15 17:29:00 +0530

    commit6

--- Loops/script.js
+++ Loops/script.js
@@ -1,3 +1,40 @@
 // if we know from where we should start and to where we should go -> for loop
 //if we know from where we should start and when we should stop -> while
- 
+ //print 1 to 30 using so while loop
+//  let i=1;
+//  do{
+//    console.log(i);
+//    i++;
+//  }while(i<33)
+
+//break
+// for teermination of the loop
+//continue
+//skip this iteration
+
+// let a=prompt('Enter any number')
+// if(a%2==0){
+//     console.log("It is even number")
+// }
+// else{
+//     console.log("It is odd number")
+// }
+// prompt() works in browsers, not in Node.js.
+// In Node, you need to use something like readline or prompt-sync to take input from the terminal.
+
+// print() is not a standard JavaScript function in either browser or Node.
+// // You should use console.log() instead.
+// let count=0;
+// for(let i=0;i<101;i++){
+//   if(count==5){
+//     break;
+//   }
+//   if(i%2 != 0){
+//      console.log(i);
+//      count++;
+//   }
+// }
+// console.log(Math.trunc(593.59085083)) //593
+// console.log(Math.floor(56.5893)) //56
+// console.log(Math.ceil(56.5893)) //57
+// console.log(Math.round(4.504)) //5
