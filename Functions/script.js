ce3b308ef797cc944860d458204a6201ac621383
CodeExplorer4267 <rupambhadra478@gmail.com>
2025-08-16 20:35:48 +0530

    commit8

--- Functions/script.js
+++ Functions/script.js
@@ -56,3 +56,15 @@
 // add(4) // it will take a as 4 -> 14
 // add(9,4) //18 -> it will take a as 9 and b as 4
 
+//rest operator
+// //if we pass high number of arguments then in parameter section we have to write all the parameters in order to solve that we can use rest operator
+// function abcd(...val){
+//     console.log(val); //in this case the output will be an array
+// }
+// abcd(1,2,3,4,5,6,7,8,9)
+// //if this ...operator works with arrays then it will be called spread operator\
+
+// function abcd(a,b,c,d,...val){
+//   console.log(val); // 5 6 7 8 9 
+//  }
+//  abcd(1,2,3,4,5,6,7,8,9);
